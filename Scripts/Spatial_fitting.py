0dac1b2012fc45e541c5758365c7d315e7cc3c2a
Mona Buisson-Fenet <buisson-fenet@is.mpg.de>
2019-02-26 14:59:23 +0100

    Created spatial fitting script

---
+++ Scripts/Spatial_fitting.py
@@ -0,0 +1,14 @@
+import numpy as np
+import pandas as pd
+
+def data_from_video(filename1, filename2):
+
+    return data1, data2
+
+
+def data_from_mocap(filename1, filename2):
+    return data1, data2
+
+
+if __name__ == '__main__':
+
