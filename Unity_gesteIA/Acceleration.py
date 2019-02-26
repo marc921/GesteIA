fafffae6b2d6a06ac43e9f94faad24715b9ca68f
Pierre-Nicolas Piquin <pierre-nicolas.piquin@mines-paristech.fr>
2019-02-26 14:05:13 +0100

    Add some plot labels

--- Unity_gesteIA/Acceleration.py
+++ Unity_gesteIA/Acceleration.py
@@ -4,7 +4,7 @@ import math
 import matplotlib.pyplot as plt
 import numpy as np
 
-from utils import norm, get_velocity, get_acc, get_acc_2, convolve_xy, dt
+from utils import norm, get_velocity_norm, get_acc, get_acc_2, convolve_xy, dt, get_gaussian_kernel, get_velocity
 
 # Parameters
 width = 480
@@ -13,7 +13,6 @@ openpose_files_path = os.path.join('.', 'OpenposeFiles')
 scale_factor = 10000
 left_hand_idx = 7
 right_hand_idx = 4
-gaussian_kernel_7 = [0.006, 0.061, 0.242, 0.383, 0.242, 0.061, 0.006]
 
 
 def process_raw_list(l):
@@ -80,26 +79,62 @@ def process_folder(folder_path):
     return person_1, person_2
 
 
+# Process Openpose output files
 person_1, person_2 = process_folder(openpose_files_path)
 
+# Position plotting
 t = [dt*i for i in range(len(person_1["left_hand"]["y"]))]
 
-x_rh1, y_rh1 = convolve_xy(person_1["right_hand"]["x"], person_1["right_hand"]["y"], gaussian_kernel_7)
-x_lh1, y_lh1 = convolve_xy(person_1["left_hand"]["x"], person_1["left_hand"]["y"], gaussian_kernel_7)
-x_rh2, y_rh2 = convolve_xy(person_2["right_hand"]["x"], person_2["right_hand"]["y"], gaussian_kernel_7)
-x_lh2, y_lh2 = convolve_xy(person_2["left_hand"]["x"], person_2["left_hand"]["y"], gaussian_kernel_7)
+gaussian_kernel = get_gaussian_kernel(7)
+x_rh1, y_rh1 = convolve_xy(person_1["right_hand"]["x"], person_1["right_hand"]["y"], gaussian_kernel)
+x_lh1, y_lh1 = convolve_xy(person_1["left_hand"]["x"], person_1["left_hand"]["y"], gaussian_kernel)
+x_rh2, y_rh2 = convolve_xy(person_2["right_hand"]["x"], person_2["right_hand"]["y"], gaussian_kernel)
+x_lh2, y_lh2 = convolve_xy(person_2["left_hand"]["x"], person_2["left_hand"]["y"], gaussian_kernel)
 
-plt.plot(t, y_rh1, 'r-')
-plt.plot(t, y_lh1, 'b-')
+plt.plot(t, y_rh1, 'r-', label='Right hand 1')
+plt.plot(t, y_lh1, 'b-', label='Left hand 1')
+plt.legend(loc='upper left')
 plt.show()
 
-a_r1 = get_acc_2(x_rh1, y_rh1)
-a_l1 = get_acc_2(x_lh1, y_lh1)
-a_r2 = get_acc_2(x_rh2, y_rh2)
-a_l2 = get_acc_2(x_lh2, y_lh2)
+# Velocity computing and plotting
+v_r1 = get_velocity_norm(x_rh1, y_rh1)
+v_l1 = get_velocity_norm(x_lh1, y_lh1)
+
+v_r1_x, v_r1_y = get_velocity(x_rh1, y_rh1)
+v_l1_x, v_l1_y = get_velocity(x_lh1, y_lh1)
+
+t_v = [dt*i for i in range(len(v_r1))]
+
+plt.plot(t_v, v_r1, 'r-', label='Right hand velocity 1')
+plt.plot(t_v, v_l1, 'g-', label='Left hand velocity 1')
+plt.legend(loc='upper left')
+plt.show()
+
+plt.plot(t_v, v_r1_y, 'r-', label='Right hand velocity 1')
+plt.plot(t_v, v_l1_y, 'g-', label='Left hand velocity 1')
+plt.legend(loc='upper left')
+plt.show()
+
+# Acceleration computing and plotting
+a_r1 = get_acc(x_rh1, y_rh1)
+a_l1 = get_acc(x_lh1, y_lh1)
+a_r2 = get_acc(x_rh2, y_rh2)
+a_l2 = get_acc(x_lh2, y_lh2)
 
 t_a = [dt*i for i in range(len(a_r1))]
 
-plt.plot(t_a, a_l1, 'r-')
-plt.plot(t_a, a_l2, 'b-')
+plt.plot(t_a, a_l1, 'r-', label='Left hand acceleration 1')
+# plt.plot(t_a, a_l2, 'b-', label='Left hand acceleration 2')
+plt.plot(t_a, a_r1, 'g-', label='Right hand acceleration 1')
+# plt.plot(t_a, a_r2, 'p-', label='Right hand acceleration 2')
+plt.legend(loc='upper left')
+plt.show()
+
+# Trying something
+a_r1_x, a_r1_y = get_velocity(v_r1_x, v_r1_y)
+a_l1_x, a_l1_y = get_velocity(v_l1_y, v_l1_y)
+
+plt.plot(t_a, a_r1_y, 'r-', label='Right hand acceleration 1')
+plt.plot(t_a, a_l1_y, 'g-', label='Left hand acceleration 1')
+plt.legend(loc='upper left')
 plt.show()
--- Unity_gesteIA/utils.py
+++ Unity_gesteIA/utils.py
@@ -9,7 +9,25 @@ def norm(x, y):
     return math.sqrt(x**2 + y**2)
 
 
+def gauss(x):
+    return math.exp(- x**2) / math.sqrt(2 * math.pi)
+
+
+def get_gaussian_kernel(size=3):
+    l = np.linspace(-1, 1, num=size)
+    res = np.array([gauss(e) for e in l])
+    return res / np.linalg.norm(res)
+
+
 def get_velocity(x, y):
+    vx, vy = [], []
+    for i in range(len(x) - 1):
+        vx += [(x[i+1] - x[i]) / dt]
+        vy += [(y[i+1] - y[i]) / dt]
+    return vx, vy
+
+
+def get_velocity_norm(x, y):
     v = []
     for i in range(len(x) - 1):
         vx = (x[i+1] - x[i]) / dt
@@ -18,7 +36,8 @@ def get_velocity(x, y):
     return v
 
 
-def get_acc(v):
+def get_acc(x, y):
+    v = get_velocity_norm(x, y)
     acc = []
     for i in range(len(v) - 1):
         acc += [(v[i+1] - v[i]) / dt]
