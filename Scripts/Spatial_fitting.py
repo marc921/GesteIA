6c176b0307261c03ac1f33136a674da0d7dfddf1
Mona Buisson-Fenet <buisson-fenet@is.mpg.de>
2019-03-04 23:34:52 +0100

    Reading tuples csv correctly, applying 1st projection method (finally!!). Now apply optimization on top of simple projection.

--- Scripts/Spatial_fitting.py
+++ Scripts/Spatial_fitting.py
@@ -1,55 +1,103 @@
 import numpy as np
 import pandas as pd
+import re
+from functools import partial
+
 
 # Take temporally fitted data and fit it spatially
 
-def data_from_csv(filename1, filename2, sep=',', video=False):
-    df1 = pd.read_csv(filename1, sep=sep)
-    df2 = pd.read_csv(filename2, sep=sep)
+def parse_this(s: str):
+    try:
+        x = float(s)
+        return x
+    except:
+        try:
+            x = tuple([float(re.sub('[)( ]', '', e)) for e in s.split(',')])
+            return x
+        except:
+            return s
+
+
+def convert_to_tuple(element):
+    try:
+        element = element.str.lstrip(' ()').str.rstrip(' ()')
+        x = [float(e) for e in element.str.split(',')]
+        return x
+    except:
+        try:
+            element = element.lstrip(' ()').rstrip(' ()')
+            x = [float(e) for e in element.split(',')]
+            return x
+        except:
+            return element
+
+
+def data_from_csv(filename1, filename2, sep=';', video=False):
+    df1 = pd.read_csv(filename1, sep=sep, converters={i: str for i in range(
+        500)})
+    df2 = pd.read_csv(filename2, sep=sep, converters={i: str for i in range(
+        500)})
     df1 = df1.dropna()
     df2 = df2.dropna()
     if video == True:
-        df1 = df1.ix[:, 1:]
-        df2 = df2.ix[:, 1:]
+        df1 = df1.iloc[:, 1:]
+        df2 = df2.iloc[:, 1:]
+    df1.apply(convert_to_tuple)
+    df2.apply(convert_to_tuple)
     return df1, df2
 
-def projection(df1_3d, df2_3d, vertical, label='Mid_hip'):
-    hip1 = np.array(df1_3d[label].iloc[0].values)
-    hip2 = np.array(df2_3d[label].iloc[0].values)
+
+def projection(df1_3d, df2_3d, vertical, label='Hip'):
+    hip1 = np.asarray(convert_to_tuple(df1_3d[label].values[0]))
+    hip2 = np.asarray(convert_to_tuple(df2_3d[label].values[0]))
     people_vector = hip1 - hip2
     cross = np.cross(people_vector, vertical)
     print(hip1, hip2, people_vector, cross)
-    B = np.concatenate((people_vector, vertical, cross), axis=1)
-    P = np.array([1, 0, 0], [0, 1, 0])
+    B = np.column_stack((people_vector, vertical, cross))
+    P = np.array([[1, 0, 0], [0, 1, 0]])
+    print('B: ', B)
+    print('P: ', P)
 
     # Define corresponding 2D dataframes after projection
-    projected_df1, projected_df2 = df1_3d, df2_3d
-    # z1_cols = [c for c in projected_df1.columns if c.lower()[-2:] != '_z']
-    # projected_df1 = projected_df1[z1_cols]
-    # z2_cols = [c for c in projected_df2.columns if c.lower()[-2:] != '_z']
-    # projected_df2 = projected_df2[z2_cols]
-    for col in df1_3d.columns:
-        point = np.array(df1_3d[col])
-        projected_df1[col] = np.dot(np.dot(P, np.inv(B)), point)
-    for col in df2_3d.columns:
-        point = np.array(df2_3d[col])
-        projected_df2[col] = np.dot(np.dot(P, np.inv(B)), point)
+    projected_df1, projected_df2 = df1_3d.copy(True), df2_3d.copy(True)
+    projected_df1 = projected_df1.apply(convert_to_tuple)
+    projected_df2 = projected_df2.apply(convert_to_tuple)
 
+    def projection(x, P, B):
+        try:
+            res = np.dot(np.dot(P, np.linalg.inv(B)), np.asarray(x,
+                                                                 dtype=np.float64))
+            return res
+        except:
+            return x
+
+    for col in projected_df1.columns:
+        if col == 'Type' or col == 'Time':
+            continue
+        projected_df1[col] = projected_df1[col].apply(projection, args=(P, B))
+    for col in projected_df2.columns:
+        if col == 'Type' or col == 'Time':
+            continue
+        projected_df1[col] = projected_df2[col].apply(projection, args=(P, B))
     return projected_df1, projected_df2
+
 
 def rescale(df):
     return rescaled_df
 
-def fit(df1, df2):
 
+def fit(df1, df2):
     return transformed_df1, df2
 
 
 if __name__ == '__main__':
-    video1, video2 = data_from_csv('../Data/video_coordinates_1.csv',
-                                   '../Data/video_coordinates_2.csv',
-                                   video=True)
+    video1, video2 = data_from_csv('../Data/video_coordinates_tuples_1.csv',
+                                   '../Data/video_coordinates_tuples_1.csv',
+                                   video=True, sep=';')
     print(video1)
-    mocap1, mocap2 = data_from_csv('../Data/Mocap_1.csv',
-                                   '../Data/Mocap_2.csv', sep=';')
+    mocap1, mocap2 = data_from_csv('../Data/Mocap_tuples1.csv',
+                                   '../Data/Mocap_tuples2.csv', sep=';')
     print(mocap1)
+
+    vertical = np.array([0, 1, 0])  # TODO check which vector is vertical
+    mocap1_2d, mocap2_2d, = projection(mocap1, mocap2, vertical, label='Hip')
