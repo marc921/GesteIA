6fb137da25067d0a8c997a0745b24485e7ffdc73
Pierre-Nicolas Piquin <pierre-nicolas.piquin@mines-paristech.fr>
2019-03-05 16:33:47 +0100

    Try to fix openpose output processing

--- Scripts/ProcessOpenposeOutput.py
+++ Scripts/ProcessOpenposeOutput.py
@@ -2,6 +2,8 @@ import os
 import json
 import numpy as np
 import pandas as pd
+import math
+from tqdm import tqdm
 
 # Settings
 scale_factor = 1
@@ -108,6 +110,10 @@ def process_folder(folder_path):
     return person_1, person_2
 
 
+def dist(x1, y1, x2, y2):
+    return math.sqrt((x1 - x2)**2 + (y1 - y2)**2)
+
+
 def export_to_csv(folder_path, write_to_disk=True, output_base_path='video_coordinates'):
     json_files = [p for p in os.listdir(folder_path) if ".json" in p]
     column_names = [name + "_x" for name in openpose_keypoints] + [name + "_y" for name in openpose_keypoints]
@@ -115,21 +121,32 @@ def export_to_csv(folder_path, write_to_disk=True, output_base_path='video_coord
     df1 = pd.DataFrame(data, columns=column_names, copy=True)
     df2 = pd.DataFrame(data, columns=column_names, copy=True)
 
-    for cpt, file in enumerate(json_files):
+    for cpt, file in tqdm(enumerate(json_files)):
         js = file = json.loads(open(os.path.join(folder_path, file), 'r').read())
         people_list = file['people']
 
         # Person 1
         x1, y1, confidence = process_raw_list(people_list[0]['pose_keypoints_2d'])
-        for i in range(len(openpose_keypoints)):
-            df1[openpose_keypoints[i] + "_x"][cpt] = x1[i]
-            df1[openpose_keypoints[i] + "_y"][cpt] = y1[i]
 
         # Person 2
         x2, y2, confidence = process_raw_list(people_list[1]['pose_keypoints_2d'])
+
         for i in range(len(openpose_keypoints)):
-            df2[openpose_keypoints[i] + "_x"][cpt] = x2[i]
-            df2[openpose_keypoints[i] + "_y"][cpt] = y2[i]
+            d1 = dist(x1[i], y1[i], df1[openpose_keypoints[i] + "_x"].values[cpt-1], df1[openpose_keypoints[i] + "_y"].values[cpt-1])
+            d2 = dist(x1[i], y1[i], df2[openpose_keypoints[i] + "_x"].values[cpt-1], df2[openpose_keypoints[i] + "_y"].values[cpt-1])
+
+            if d1 < d2:
+                df1[openpose_keypoints[i] + "_x"][cpt] = x1[i]
+                df1[openpose_keypoints[i] + "_y"][cpt] = y1[i]
+
+                df2[openpose_keypoints[i] + "_x"][cpt] = x2[i]
+                df2[openpose_keypoints[i] + "_y"][cpt] = y2[i]
+            else:
+                df1[openpose_keypoints[i] + "_x"][cpt] = x2[i]
+                df1[openpose_keypoints[i] + "_y"][cpt] = y2[i]
+
+                df2[openpose_keypoints[i] + "_x"][cpt] = x1[i]
+                df2[openpose_keypoints[i] + "_y"][cpt] = y1[i]
 
     t = [i*dt for i in range(len(json_files))]
     df1["time"] = t
@@ -144,33 +161,47 @@ def export_to_csv(folder_path, write_to_disk=True, output_base_path='video_coord
 def export_to_csv_tuples(folder_path, write_to_disk=True, output_base_path='video_coordinates_tuples'):
     json_files = [p for p in os.listdir(folder_path) if ".json" in p]
     data = np.zeros(shape=(len(json_files), len(openpose_keypoints)))
-    d1 = {}
-    d2 = {}
+    dict1 = {}
+    dict2 = {}
 
     for key in openpose_keypoints:
-        d1[key] = []
-        d2[key] = []
+        dict1[key] = []
+        dict2[key] = []
 
-    for cpt, file in enumerate(json_files):
+    for cpt, file in tqdm(enumerate(json_files)):
         js = file = json.loads(open(os.path.join(folder_path, file), 'r').read())
         people_list = file['people']
 
         # Person 1
         x1, y1, confidence = process_raw_list(people_list[0]['pose_keypoints_2d'])
-        for i in range(len(openpose_keypoints)):
-            d1[openpose_keypoints[i]] += [(x1[i], y1[i])]
 
         # Person 2
         x2, y2, confidence = process_raw_list(people_list[1]['pose_keypoints_2d'])
         for i in range(len(openpose_keypoints)):
-            d2[openpose_keypoints[i]] += [(x2[i], y2[i])]
+            if cpt > 0:
+                if x1[i] == 0 and y1[i] == height:
+                    d1 = dist(x2[i], y2[i], dict2[openpose_keypoints[i]][-1][0], dict2[openpose_keypoints[i]][-1][1])
+                    d2 = dist(x2[i], y2[i], dict1[openpose_keypoints[i]][-1][0], dict1[openpose_keypoints[i]][-1][1])
+                else:
+                    d1 = dist(x1[i], y1[i], dict1[openpose_keypoints[i]][-1][0], dict1[openpose_keypoints[i]][-1][1])
+                    d2 = dist(x1[i], y1[i], dict2[openpose_keypoints[i]][-1][0], dict2[openpose_keypoints[i]][-1][1])
+            else:
+                d1 = 0
+                d2 = 42
+
+            if d1 < d2:
+                dict1[openpose_keypoints[i]] += [(x1[i], y1[i])]
+                dict2[openpose_keypoints[i]] += [(x2[i], y2[i])]
+            else:
+                dict2[openpose_keypoints[i]] += [(x1[i], y1[i])]
+                dict1[openpose_keypoints[i]] += [(x2[i], y2[i])]
 
     t = [i*dt for i in range(len(json_files))]
-    df1 = pd.DataFrame(d1)
+    df1 = pd.DataFrame(dict1)
     df1["time"] = t
     df1.to_csv(output_base_path + "_1.csv", index=False, sep=";")
 
-    df2 = pd.DataFrame(d2)
+    df2 = pd.DataFrame(dict2)
     df2["time"] = t
     df2.to_csv(output_base_path + "_2.csv", index=False, sep=";")
 
@@ -181,5 +212,5 @@ openpose_files_path = os.path.join('..', 'Unity_gesteIA', 'Assets', 'Data', 'Ope
 output_path = os.path.join('..', 'Data', 'video_coordinates')
 output_path_tuples = os.path.join('..', 'Data', 'video_coordinates_tuples')
 
-export_to_csv(openpose_files_path, output_base_path=output_path)
+# export_to_csv(openpose_files_path, output_base_path=output_path)
 export_to_csv_tuples(openpose_files_path, output_base_path=output_path_tuples)
