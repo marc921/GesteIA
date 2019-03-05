fdf29209f417ec77cc28f6c6f7e68618e0255a0d
Mona Buisson-Fenet <buisson-fenet@is.mpg.de>
2019-03-05 13:35:14 +0100

    Added functions to rotate pointclouds by angle, and fit them by translating to point with certain label. Now missing optimization procedure to find best angle to rotate them by before fitting by translation

--- Scripts/Spatial_fitting.py
+++ Scripts/Spatial_fitting.py
@@ -1,6 +1,8 @@
 import numpy as np
 import pandas as pd
 import re
+import scipy as sp
+import math
 from functools import partial
 
 
@@ -47,7 +49,7 @@ def data_from_csv(filename1, filename2, sep=';', video=False):
     return df1, df2
 
 
-def projection(df1_3d, df2_3d, vertical, label='Hip'):
+def project3d_to_plane(df1_3d, df2_3d, vertical, label='Hip'):
     hip1 = np.asarray(convert_to_tuple(df1_3d[label].values[0]))
     hip2 = np.asarray(convert_to_tuple(df2_3d[label].values[0]))
     people_vector = hip1 - hip2
@@ -59,9 +61,14 @@ def projection(df1_3d, df2_3d, vertical, label='Hip'):
     print('P: ', P)
 
     # Define corresponding 2D dataframes after projection
-    projected_df1, projected_df2 = df1_3d.copy(True), df2_3d.copy(True)
-    projected_df1 = projected_df1.apply(convert_to_tuple)
-    projected_df2 = projected_df2.apply(convert_to_tuple)
+    projected_positions1, projected_positions2 = df1_3d.copy(True), \
+                                                 df2_3d.copy(True)
+    projected_positions1 = projected_positions1[projected_positions1['Type']
+                                                == 'position'].dropna()
+    projected_positions2 = projected_positions2[projected_positions2['Type']
+                                                == 'position'].dropna()
+    projected_positions1 = projected_positions1.apply(convert_to_tuple)
+    projected_positions2 = projected_positions2.apply(convert_to_tuple)
 
     def projection(x, P, B):
         try:
@@ -71,33 +78,111 @@ def projection(df1_3d, df2_3d, vertical, label='Hip'):
         except:
             return x
 
-    for col in projected_df1.columns:
+    for col in projected_positions1.columns:
         if col == 'Type' or col == 'Time':
             continue
-        projected_df1[col] = projected_df1[col].apply(projection, args=(P, B))
-    for col in projected_df2.columns:
+        projected_positions1[col] = projected_positions1[col].apply(projection,
+                                                                    args=(P, B))
+    for col in projected_positions2.columns:
         if col == 'Type' or col == 'Time':
             continue
-        projected_df1[col] = projected_df2[col].apply(projection, args=(P, B))
-    return projected_df1, projected_df2
+        projected_positions1[col] = projected_positions2[col].apply(projection,
+                                                                    args=(P, B))
+    projected_positions1 = projected_positions1.dropna()
+    projected_positions2 = projected_positions2.dropna()
+    return projected_positions1, projected_positions2
 
 
-def rescale(df):
+def rotation_matrix(axis, theta):
+    """
+    Return the rotation matrix associated with counterclockwise rotation about
+    the given axis by theta radians.
+    https://stackoverflow.com/questions/6802577/rotation-of-3d-vector
+    """
+    axis = np.asarray(axis)
+    axis = axis / np.linalg.norm(axis)
+    a = np.cos(theta / 2.0)
+    b, c, d = -axis * np.sin(theta / 2.0)
+    aa, bb, cc, dd = a * a, b * b, c * c, d * d
+    bc, ad, ac, ab, bd, cd = b * c, a * d, a * c, a * b, b * d, c * d
+    return np.array([[aa + bb - cc - dd, 2 * (bc + ad), 2 * (bd - ac)],
+                     [2 * (bc - ad), aa + cc - bb - dd, 2 * (cd + ab)],
+                     [2 * (bd + ac), 2 * (cd - ab), aa + dd - bb - cc]])
+
+
+def rotate_point(point, theta, vertical):
+    try:
+        rotated_point = np.dot(rotation_matrix(vertical, theta), point)
+        return rotated_point
+    except:
+        return point
+
+
+def rotate_data(df, theta, vertical):
+    for col in df.columns:
+        if col == 'Type' or col == 'Time':
+            continue
+        df[col] = df[col].apply(rotate_point, args=(theta, vertical))
+
+
+def rescale(df, scaling_factor):
+    for col in df.columns:
+        if col == 'Type' or col == 'Time':
+            continue
+        df[col] = df[col].apply(lambda x: scaling_factor * x)
     return rescaled_df
 
 
-def fit(df1, df2):
-    return transformed_df1, df2
+def fit_translate(mocap, video, label='Hip'):
+    T = np.asarray(convert_to_tuple(video[label].values[0])) - np.asarray(
+        convert_to_tuple(mocap[label].values[0]))
+    print(T, type(T))
+
+    def translate_point(point, T):
+        try:
+            return point + T
+        except:
+            return point
+
+    for col in mocap.columns:
+        if col == 'Type' or col == 'Time':
+            continue
+        mocap[col] = mocap[col].apply(translate_point, args=T)
+    return fitted_mocap, video
+
+
+def optimize_projection(mocap, video, vertical, label='Hip'):
+    # TODO
+
+    def score_projection(theta, vertical, mocap, video):
+        rotate_data(mocap, theta, vertical)
+
+    optim = sp.optimize.minimize(score_projection,
+                                 args=(vertical, mocap, video))
+    return projected_mocap
 
 
 if __name__ == '__main__':
+    # Unit tests
+    print(rotate_point(np.array([1, 0, 0]), 1.57, np.array([0, 0, 1])))
+
+    # Retrieve data
     video1, video2 = data_from_csv('../Data/video_coordinates_tuples_1.csv',
                                    '../Data/video_coordinates_tuples_1.csv',
                                    video=True, sep=';')
-    print(video1)
+    print('Video data 1: ', video1)
     mocap1, mocap2 = data_from_csv('../Data/Mocap_tuples1.csv',
                                    '../Data/Mocap_tuples2.csv', sep=';')
-    print(mocap1)
+    # Mocap coord syst: x outward from person, y vertical, z from left to
+    # right shoulder
+    print('Original mocap data 1: ', mocap1)
 
-    vertical = np.array([0, 1, 0])  # TODO check which vector is vertical
-    mocap1_2d, mocap2_2d, = projection(mocap1, mocap2, vertical, label='Hip')
+    # Test: project Mocap data directly onto 2D plane
+    vertical = np.array([0, 1, 0])
+    mocap_positions1_2d, mocap_positions2_2d, = project3d_to_plane(mocap1,
+                                                                   mocap2,
+                                                                   vertical,
+                                                                   label='Hip')
+    print('Projected mocap data 1: ', mocap_positions1_2d)
+
+    # Find optimal initial transformation of pointclouds before projection
